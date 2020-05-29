7f7c0566e102c729db82f2b529d8b834a7378b46
Nick Burgess <nburgess@uchicago.edu>
2020-05-29 09:28:30 -0500

    Move a bunch of stuff

--- get_dotfiles.sh
+++ get_dotfiles.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-git clone https://github.com/nburg/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles
+if [ -d ~/.dotfiles ]; then
+  cd ~/.dotfiles
+  git pull
+else
+  git clone https://github.com/nburg/dotfiles.git ~/.dotfiles
+fi
+
+cd ~/.dotfiles/home
 for i in `ls -A`; do
   if [ -e ~/$i ]; then
     rm -rf ~/$i
   fi
-  ln -s ~/.dotfiles/$i ~/$i
+  ln -s ~/.dotfiles/home/$i ~/$i
 done
+source ~/.bashrc
