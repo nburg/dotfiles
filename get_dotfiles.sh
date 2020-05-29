3208d62c0607d4d468809beb5fd3b1cd4ca19345
Nick Burgess <nburgess@uchicago.edu>
2020-05-29 09:07:39 -0500

    Add script to download this biz

---
+++ get_dotfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/nburg/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+for i in `ls -A`; do
+  if [ -e $i ]; then
+    rm $i
+  fi
+  ln -s ~/.dotfiles/$i ~/$i
+done
